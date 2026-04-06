351cbd5b4e6e8f3808f918d88f7b05e9a97bbd7f
alanjoshy <alanjoshy@codeteak.com>
2026-04-06 16:39:17 +0530

    fIX:fixed the bucket upload issue

--- src/main/bootstrap.js
+++ src/main/bootstrap.js
@@ -2,12 +2,25 @@ import { env } from "../config/env.js";
 import { logger } from "../config/logger.js";
 import { createServer } from "./server.js";
 import { pool } from "../infra/db/pool.js";
+import { isR2Configured } from "../infra/media/storeR2Object.js";
 
 async function main() {
   const app = createServer();
 
   // Startup DB check
   await pool.query("select 1 as ok");
+
+  if (isR2Configured()) {
+    logger.info(
+      { shopImages: "cloudflare_r2", bucket: env.OBJECT_STORAGE_BUCKET },
+      "Shop entity images will be stored in R2"
+    );
+  } else {
+    logger.info(
+      { shopImages: "local_disk", uploadsDir: env.UPLOADS_DIR },
+      "Shop entity images will be stored locally (set OBJECT_STORAGE_* for R2)"
+    );
+  }
 
   app.listen(env.PORT, () => {
     logger.info({ port: env.PORT }, "Admin API listening");
